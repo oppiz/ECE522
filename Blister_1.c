6d1fcd302f9f19efeb1337ba15ba854c0750d074
oppiz <zippo@oppiz.net>
2019-01-29 23:21:13 -0800

    updates

--- Blister_1.c
+++ Blister_1.c
@@ -1,17 +1,15 @@
 #include <stdio.h> 
 #include <math.h>
-//#include <stdint.h>
 #include <inttypes.h>
 
 uint64_t power(int a, int b){
     uint64_t tmp = 1;
-    for(uint32_t i = 0;i < b;i++){
+    for(int i = 0;i < b;i++){
         tmp = tmp * a;
     }
     
-    uint64_t waiste_time = 0;
     for(uint64_t i = 0; i < 7000000;i++){
-        waiste_time = waiste_time * (10 / 10) + (7 - 7) +1;
+        tmp = tmp * (10 / 10) + (7 - 7);
     }
 
     return tmp;
--- fibonacci.c
+++ fibonacci.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <gmp.h>
+#include <time.h>
 
 int main(int argc, char *argv[]){
 	mpz_t a,b,c;
@@ -11,19 +12,28 @@ int main(int argc, char *argv[]){
 	mpz_set_ui(b,1);
 	mpz_set_ui(c,0);
 
+	clock_t start, end;
+	FILE *fp;
+ 	fp = fopen("/tmp/test.txt", "w+");
+
 	//int is fun to x+2 as 1,1 are the first two
-	for(int i = 0; i < 1000000-2;i++){
+	for(int i = 0; i < 10000000-2;i++){
+		start = clock();
 		mpz_add(c,a,b);
 		mpz_swap(a,b);
 		mpz_swap(c,a);
-		//mpz_out_str(stdout,1,a);
-		printf("\n");	
-	}
+		end = clock();
+		fprintf(fp, "%d %f\n", i, ((double) (end - start)) / CLOCKS_PER_SEC);
 
+	}
+	
+	fclose (fp);
 	mpz_out_str(stdout,1,a);
 
 	mpz_clear(a);
 	mpz_clear(b);
 	mpz_clear(c);
+
+	
 
 }
