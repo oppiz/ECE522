07b4ab536ba5f0f854df88b472464e77a514a378
oppiz <zippo@oppiz.net>
2019-02-16 19:50:40 -0800

    now works with python

--- main.c
+++ main.c
@@ -8,10 +8,12 @@ int main(int argc, char **argv) {
 	int a = Modbus_init();
 	int b = Datastore_init();
 
-	printf("%d %d\n", a, b);
+	//printf("%d %d\n", a, b);
 
-	for(int i = 0; i < 100; i++){
-		printf("%d \n", i);
+	//for(int i = 0; i < 100; i++){
+
+	while(1){
+		//printf("%d \n", i);
 		Modbus_Read p = Modbus_read();
 		int c = Datastore_insert(p);
 		sleep(3);
